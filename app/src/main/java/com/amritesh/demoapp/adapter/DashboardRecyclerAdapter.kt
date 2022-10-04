c6fd062749a181810219b18af5a07143bea0d538
Amritesh <48851357+amriteshgupta@users.noreply.github.com>
2022-10-04 18:08:35 +0530

    Update DashboardRecyclerAdapter.kt

--- app/src/main/java/com/amritesh/demoapp/adapter/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/amritesh/demoapp/adapter/DashboardRecyclerAdapter.kt
@@ -10,21 +10,21 @@ import com.amritesh.demoapp.R
 
 class DashboardRecyclerAdapter(val context: Context,val itemList: ArrayList<String>): RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {
 
-    class DashboardViewHolder(view: View): RecyclerView.ViewHolder(view){
-        val textView:TextView = view.findViewById(R.id.txtBookName)
-    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_dashboard_single_row,parent,false)
 
         return DashboardViewHolder(view)
     }
-
-    override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
-        val text= itemList[position]
-    }
-
     override fun getItemCount(): Int {
         return itemList.size
+    }
+    override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
+        val text= itemList[position]
+        holder.textView.text = text
+    }
+    class DashboardViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val textView:TextView = view.findViewById(R.id.txtBookName)
     }
 }
